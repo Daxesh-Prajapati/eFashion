1a332d3ef2fdc1dcdb7b4e80c44e77c38b58532d
Daxesh-Prajapati <daxeshhh.p@gmail.com>
2025-02-21 10:06:18 +0530

    Vendor Components Created

--- eFashion/src/App.jsx
+++ eFashion/src/App.jsx
@@ -8,17 +8,23 @@ import { UserSidebar } from './components/layouts/UserSidebar';
 import { Route, Routes } from 'react-router-dom';
 import { Login } from './components/common/Login';
 import { Signup } from './components/common/Signup';
+import { AddProduct } from './components/vendor/AddProduct';
+import { VendorSidebar } from './components/vendor/VendorSidebar';
+import { VendorNavbar } from './components/vendor/VendorNavbar';
 // import './App.css'
 
 function App() {
   return (
     <body className="layout-fixed sidebar-expand-lg bg-body-tertiary app-loaded sidebar-open">
       <div className="app-wrapper">
-       <Routes>
-        <Route path='/user' element={<UserSidebar/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-       </Routes>
+        <Routes>
+          <Route path="/user" element={<UserSidebar />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/AddProduct" element={<AddProduct />}></Route>
+          <Route path="/VendorSidebar" element={<VendorSidebar />}></Route>
+          <Route path="/VendorNavbar" element={<VendorNavbar />}></Route>
+        </Routes>
       </div>
     </body>
   );
---
+++ eFashion/src/components/vendor/AddProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddProduct = () => {
+  return (
+    <div>
+        <h1>AddProduct
+            
+        </h1>
+    </div>
+  )
+}
